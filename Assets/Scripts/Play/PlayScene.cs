fe59022546dd2444aadbdfbff53697b1ba0cd983
kamatayosaimin <haneishi4622@gmail.com>
2022-02-08 02:17:27 +0900

    UI制御機能実装

--- Assets/Scripts/Play/PlayScene.cs
+++ Assets/Scripts/Play/PlayScene.cs
@@ -1,20 +1,56 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayScene : SceneBehaviour
 {
+
+    [Serializable]
+    public class HpUI
+    {
+        [SerializeField] private Slider _slider;
+        [SerializeField] private Text _text;
+
+        public void SetUI(int hp)
+        {
+            _slider.value = hp;
+
+            _text.text = hp.ToString();
+        }
+    }
+
+    [Serializable]
+    public class ScoreText
+    {
+        [SerializeField] private string _style = "SC {0:D8}";
+        [SerializeField] private Text _text;
+
+        public void SetText(int score)
+        {
+            _text.text = string.Format(_style, score);
+        }
+    }
+
+    [SerializeField] private int _maxScore = 99999990;
+    private int _score;
     private PlayState _state;
     [SerializeField] private AudioSource[] _sounds;
     [SerializeField] private Generator _generator;
     [SerializeField] private PlayerController _playerController;
+    [SerializeField] private HpUI _hpUI;
+    [SerializeField] private ScoreText _scoreText;
 
     // Start is called before the first frame update
     protected override void Start()
     {
         base.Start();
 
+        _score = 0;
         _state = PlayState.Start;
+
+        _scoreText.SetText(_score);
 
         AtsumaruManager.Comment.SetContext("Play");
     }
@@ -54,8 +90,16 @@ public class PlayScene : SceneBehaviour
         {
             _sounds[index].Play();
         }
-        catch (System.IndexOutOfRangeException e)
+        catch (IndexOutOfRangeException e)
         {
         }
+    }
+
+    public void AddScore(int add)
+    {
+        _score += add;
+        _score = Mathf.Min(_score, _maxScore);
+
+        _scoreText.SetText(_score);
     }
 }
